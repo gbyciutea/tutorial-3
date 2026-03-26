90932297676e4000ddc8bf15a196b7e1f4d2c28e
gbyciutea <byce.gabe@icloud.com>
2026-03-26 03:20:23 +0200

    from 300 to 200

--- 1.Matrix hackers screen.py
+++ 1.Matrix hackers screen.py
@@ -2,7 +2,7 @@ import random
 import time
 chars = "01"
 while True:
-    line="".join(random.choice (chars) for i in range(300))
+    line="".join(random.choice (chars) for i in range(200))
     print(line)
     time.sleep(0.05)
 
